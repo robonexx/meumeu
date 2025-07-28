38ac2ce8e290b4d049cd818a12278b5676016458
robonexx <robertwagar@gmail.com>
2025-07-28 17:42:51 +0200

    ... meu meu video bg

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,30 +1,32 @@
 // components/Nav.tsx
 import Link from 'next/link';
+
 export default function Nav() {
   return (
-    <nav className='flex sm:flex-row gap-6 align-center justify-between p-6 bg-black dark:bg-gray-800 shadow-md rounded-lg'>
+    <nav className='w-full bg-black/90 dark:bg-gray-900 shadow-md px-2 py-2 flex flex-row items-center justify-between'>
+      {/* Brand */}
       <Link
         href='/'
-        className='px-8 py-4 bg-gray-800 dark:bg-gray-700 text-white rounded-lg shadow-lg hover:bg-gray-700 dark:hover:bg-gray-600 transition'
+        className='font-bold tracking-tight text-base xs:text-lg sm:text-xl md:text-2xl px-2 md:px-4 py-2 text-white'
       >
         Meu Meu
       </Link>
-      <div className='flex flex-col sm:flex-row gap-6 align-center justify-center'>
+      <div className='flex flex-row items-center gap-2 xs:gap-3 sm:gap-5'>
         <Link
           href='/moon'
-          className='px-8 py-4 bg-gray-800 dark:bg-gray-700 text-white rounded-lg shadow-lg hover:bg-gray-700 dark:hover:bg-gray-600 transition'
+          className='px-2 md:px-4 py-1.5 md:py-2 text-white bg-gray-800 dark:bg-gray-700 rounded-lg hover:bg-gray-700 dark:hover:bg-gray-600 transition text-sm xs:text-base md:text-lg'
         >
           Moon
         </Link>
         <Link
           href='/sun'
-          className='px-8 py-4 bg-[goldenrod] dark:bg-[gold] text-yellow-900 dark:text-yellow-900 rounded-lg shadow-lg hover:bg-[gold] dark:hover:bg-yellow-600 transition'
+          className='px-2 md:px-4 py-1.5 md:py-2 text-yellow-900 bg-yellow-300 dark:bg-yellow-500 rounded-lg hover:bg-yellow-400 dark:hover:bg-yellow-400 transition text-sm xs:text-base md:text-lg'
         >
           Sun
         </Link>
         <Link
           href='/gallery'
-          className='px-8 py-4 bg-[#282828] dark:bg-[#181818] text-[goldenrod] dark:text-blue-900 rounded-lg shadow-lg hover:bg-[#181818] dark:hover:text-[gold] transition'
+          className='px-2 md:px-4 py-1.5 md:py-2 text-yellow-400 bg-gray-900 dark:bg-gray-800 rounded-lg hover:bg-gray-800 dark:hover:bg-gray-700 transition text-sm xs:text-base md:text-lg'
         >
           Gallery
         </Link>
